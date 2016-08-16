d5a7b22e20186aa5c2f918d04092507b5ba92bd7
jianghong <790712946@qq.com>
2016-08-16 14:25:09 +0800

    初次使用spring缓存策略，查询Users表

--- src/main/java/com/example/model/test/repository/UsersRepository.java
+++ src/main/java/com/example/model/test/repository/UsersRepository.java
@@ -1,6 +1,7 @@
 package com.example.model.test.repository;
 
 import com.example.model.test.domain.Users;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,12 @@ import org.springframework.data.repository.query.Param;
  * Created by jianghong on 2016/8/7.
  */
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    /**
+     * 对该次查询添加缓存
+     * @param name
+     * @return
+     */
+    @Cacheable
     Users findByName(String name);
 
     Users findByNameAndAge(String name, Integer age);
